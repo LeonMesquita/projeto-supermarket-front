3913bc06c810fe419360cc1a84a1424175ca6111
LeonMesquita <leonmesquita16@gmail.com>
2022-10-08 17:18:29 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import dotenv from 'dotenv';
 // dotenv.config()
-//
+//https://projeto-autoral.onrender.com
 //http://localhost:5000
 const instance = axios.create({
   baseURL: "https://projeto-autoral.onrender.com"
